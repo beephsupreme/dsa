4e0842b692b3568443524c30792a43b204c77438
Michael Rowsey <mike@rowsey.org>
2024-02-01 06:20:16 -0800

    add compiler warnings, use fmt::format in main.cc

--- app/main.cc
+++ app/main.cc
@@ -1,34 +1,31 @@
-#include <iostream>
+#include <fmt/format.h>
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 #include <cxxopts.hpp>
-#include <fmt/format.h>
 #include "config.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
 
-  int i;
+  std::string s = fmt::format("{} {}", project_name, project_version);
+  cout << s << endl;
 
-  cout << project_name << " " << project_version << endl;
+  s = fmt::format("JSON: {}.{}.{}",
+                  NLOHMANN_JSON_VERSION_MAJOR,
+                  NLOHMANN_JSON_VERSION_MINOR,
+                  NLOHMANN_JSON_VERSION_PATCH);
+  cout << s << endl;
 
-  cout << "JSON: "
-       << NLOHMANN_JSON_VERSION_MAJOR << "."
-       << NLOHMANN_JSON_VERSION_MINOR << "."
-       << NLOHMANN_JSON_VERSION_PATCH << endl;
+  s = fmt::format("CXXOPTS: {}.{}.{}", CXXOPTS__VERSION_MAJOR, CXXOPTS__VERSION_MINOR, CXXOPTS__VERSION_PATCH);
+  cout << s << endl;
 
-  cout << "CXXOPTS: "
-       << CXXOPTS__VERSION_MAJOR << "."
-       << CXXOPTS__VERSION_MINOR << "."
-       << CXXOPTS__VERSION_PATCH << endl;
+  s = fmt::format("SPDLOG: {}.{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+  cout << s << endl;
 
-  cout << "SPDLOG: "
-       << SPDLOG_VER_MAJOR << "."
-       << SPDLOG_VER_MINOR << "."
-       << SPDLOG_VER_PATCH << endl;
-
-  cout << "FMT: " << FMT_VERSION << endl;
+  s = fmt::format("FMT: {}", FMT_VERSION);
+  cout << s << endl;
 
   return 0;
 }
