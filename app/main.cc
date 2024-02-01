b913ea5766deebe581b9d48414dcd8e173b6acb4
Michael Rowsey <mike@rowsey.org>
2024-01-31 22:46:28 -0800

    refactor, remove essentials/recursion, add lohman/fmt/cxxopts/spdlog

--- app/main.cc
+++ app/main.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
-#include "adt.h"
 #include "config.h"
 
 using namespace std;
@@ -11,16 +10,5 @@ int main() {
        << NLOHMANN_JSON_VERSION_MAJOR << "."
        << NLOHMANN_JSON_VERSION_MINOR << "."
        << NLOHMANN_JSON_VERSION_PATCH << endl;
-  Array arr;
-  arr.append(23);
-  arr.append(24);
-  arr.append(32);
-  arr.append(8);
-  arr.append(42);
-  arr.display();
-  cout << (arr.isSorted() ? "sorted" : "not sorted") << endl;
-  arr.sort();
-  arr.display();
-  cout << (arr.isSorted() ? "sorted" : "not sorted") << endl;
   return 0;
 }
