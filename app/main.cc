52e44dc57379ba23edaebe8372272ee477c60bc9
Michael Rowsey <mike@rowsey.org>
2024-02-02 03:59:39 -0800

    fixed spdlog error, changed app to use imported libraries

--- app/main.cc
+++ app/main.cc
@@ -6,29 +6,47 @@
 #include <iostream>
 
 using namespace std;
+using json = nlohmann::json;
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  int x[2];
-  x[2] = 1337;
+  cxxopts::Options options("MyProgram", "One line description of MyProgram");
 
-  std::string s = fmt::format("{} {}", project_name, project_version);
-  cout << s << endl;
+  options.add_options()
+      ("d,debug", "Enable debugging") // a bool parameter
+      ("i,integer", "Int param", cxxopts::value<int>())
+      ("f,file", "File name", cxxopts::value<std::string>())
+      ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
 
-  s = fmt::format("JSON: {}.{}.{}",
-                  NLOHMANN_JSON_VERSION_MAJOR,
-                  NLOHMANN_JSON_VERSION_MINOR,
-                  NLOHMANN_JSON_VERSION_PATCH);
-  cout << s << endl;
+  cout << fmt::format("Welcome to {} {} !", project_name, project_version) << endl;
 
-  s = fmt::format("CXXOPTS: {}.{}.{}", CXXOPTS__VERSION_MAJOR, CXXOPTS__VERSION_MINOR, CXXOPTS__VERSION_PATCH);
-  cout << s << endl;
+  auto result = options.parse(argc, argv);
 
-  s = fmt::format("SPDLOG: {}.{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
-  cout << s << endl;
+  try {
+    if (result["debug"].as<bool>()) {
+      spdlog::set_level(spdlog::level::debug);
+    }
+  } catch (const cxxopts::exceptions::exception &e) {
+    spdlog::error("cxxopts error: {}", e.what());
+  }
+  spdlog::debug("JSON: {}.{}.{}",
+                NLOHMANN_JSON_VERSION_MAJOR,
+                NLOHMANN_JSON_VERSION_MINOR,
+                NLOHMANN_JSON_VERSION_PATCH);
+  spdlog::debug("CXXOPTS: {}.{}.{}", CXXOPTS__VERSION_MAJOR, CXXOPTS__VERSION_MINOR, CXXOPTS__VERSION_PATCH);
+  spdlog::debug("SPDLOG: {}.{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+  spdlog::debug("FMT: {}", FMT_VERSION);
 
-  s = fmt::format("FMT: {}", FMT_VERSION);
-  cout << s << endl;
+  cout << "Creating json object ..." << endl;
+  json j;
+  j["pi"] = 3.141;
+  j["happy"] = true;
+  j["name"] = "Niels";
+  j["nothing"] = nullptr;
+  j["answer"]["everything"] = 42;
+  j["list"] = { 1, 0, 2 };
+  j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+  cout << "...done." << endl;
 
   return 0;
 }
